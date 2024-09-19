9ba85db41989bb927dd738cda0422642f2673a2f
Rodrigo <rlopezpatino@gmail.com>
2024-09-19 11:49:26 -0700

    setting up brewing

--- IdlePotionMaker/Assets/Scripts/Cauldron.cs
+++ IdlePotionMaker/Assets/Scripts/Cauldron.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cauldron : MonoBehaviour
 {
     private int ingredients;
     private bool full;
+
+    public Text brewtext;
+    public int brewTime;
+
     // Start is called before the first frame update
     void Start()
     {
         full = false;
+        brewtext = null;
     }
 
     // Update is called once per frame
@@ -18,6 +25,12 @@ public class Cauldron : MonoBehaviour
         if(ingredients >= 2)
         {
             full=true;
+            ChangeText();
+            StartCoroutine(CountDown());
+            if(brewTime == 0) 
+            {
+                brewtext.text = "Done";
+            }
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -27,5 +40,16 @@ public class Cauldron : MonoBehaviour
             other.gameObject.SetActive(false);
             ingredients++;
         }
+    }
+
+    private IEnumerator CountDown()
+    {
+        brewTime--;
+        yield return new WaitForSeconds(1);
+    }
+
+    private void ChangeText()
+    {
+        brewtext.text = "Brewing: " + brewTime.ToString();
     }
 }
