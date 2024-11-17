f001b46b10b7a9b7407f78486ab8546497ef09bf
Rodrigo <rlopezpatino@gmail.com>
2024-11-17 00:38:52 -0800

    Shop Popup
    if in the shop, spawn ingredients. if not enough money, displays a popup that fades

--- IdlePotionMaker/Assets/Scripts/IngredientSpawner.cs
+++ IdlePotionMaker/Assets/Scripts/IngredientSpawner.cs
@@ -24,8 +24,11 @@ public class IngredientSpawner : MonoBehaviour
 
     private void OnMouseDown()
     {
-        GoldCounter.instance.DecreaseGold(cost);
-        GameObject newIn = Instantiate(IngredientPrefab);
-        newIn.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - .5f);
+        if(ShopHandler.inShop == false)
+        {
+            GoldCounter.instance.DecreaseGold(cost);
+            GameObject newIn = Instantiate(IngredientPrefab);
+            newIn.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - .5f);
+        }
     }
 }
--- IdlePotionMaker/Assets/Scripts/ShopHandler.cs
+++ IdlePotionMaker/Assets/Scripts/ShopHandler.cs
@@ -15,12 +15,18 @@ public class ShopHandler : MonoBehaviour
     [SerializeField]
     private Button cbutton, hbutton, tbutton, ebutton, dbutton;
 
-    private bool csold, hsold, tsold, esold, dsold;
+    private bool csold, hsold, tsold, esold, dsold, upgrade, displayPopUp, resetPop;
 
-    
+    [SerializeField]
+    private GameObject popUp;
+    [SerializeField]
+    private Image background;
+    [SerializeField]
+    private TMP_Text popupText;
 
+    private Color backgroundColor, textColor;
 
-    private bool inShop;
+    public static bool inShop;
 
     // Start is called before the first frame update
     void Start()
@@ -31,11 +37,24 @@ public class ShopHandler : MonoBehaviour
         tsold = false;
         esold = false;
         dsold = false;
+        upgrade = false;
+        resetPop = false;
+        displayPopUp = false;
+        popUp.SetActive(false);
+        backgroundColor = background.color;
+        textColor = popupText.color;
     }
 
     // Update is called once per frame
     void Update()
     {
+        background.color = backgroundColor;
+        popupText.color = textColor;
+        if (displayPopUp) 
+        {
+            BrokePopUp();
+        }
+
         if (inShop)
         {
             shopUI.SetActive(true);
@@ -53,46 +72,134 @@ public class ShopHandler : MonoBehaviour
 
     public void CauldronButton()
     {
-        c1.SetActive(false);
-        cauldron.text = "Sold";
-        cauldron.transform.localPosition = Vector3.zero;
-        csold = true;
-        cbutton.interactable = false;
+        CheckGold(int.Parse(cauldron.text));
+        if (upgrade) 
+        {
+            c1.SetActive(false);
+            cauldron.transform.localPosition = Vector3.zero;
+            csold = true;
+            cbutton.interactable = false;
+            GoldCounter.instance.DecreaseGold(int.Parse(cauldron.text));
+            cauldron.text = "Sold";
+            upgrade = false;
+        }   
+        else
+        {
+            displayPopUp = true;
+        }
     }
 
     public void UnicornButton()
     {
-        c2.SetActive(false);
-        horn.text = "Sold";
-        horn.transform.localPosition = Vector3.zero;
-        hsold = true;
-        hbutton.interactable = false;
+        CheckGold(int.Parse(horn.text));
+        if (upgrade)
+        {
+            c2.SetActive(false);
+            horn.transform.localPosition = Vector3.zero;
+            hsold = true;
+            hbutton.interactable = false;
+            GoldCounter.instance.DecreaseGold(int.Parse(horn.text));
+            horn.text = "Sold";
+            upgrade = false;
+        }
+        else
+        {
+            displayPopUp = true;
+        }
     }
 
     public void TrollButton()
     {
-        c3.SetActive(false);
-        toe.text = "Sold";
-        toe.transform.localPosition = Vector3.zero;
-        tsold = true;
-        tbutton.interactable = false;
+        CheckGold(int.Parse(toe.text));
+        if (upgrade)
+        {
+            c3.SetActive(false);
+            toe.transform.localPosition = Vector3.zero;
+            tsold = true;
+            tbutton.interactable = false;
+            GoldCounter.instance.DecreaseGold(int.Parse(toe.text));
+            toe.text = "Sold";
+            upgrade = false;
+        }
+        else
+        {
+            displayPopUp = true;
+        }
     }
 
     public void OrcButton()
     {
-        c4.SetActive(false);
-        ear.text = "Sold";
-        ear.transform.localPosition = Vector3.zero;
-        esold = true;
-        ebutton.interactable = false;
+        CheckGold(int.Parse(ear.text));
+        if (upgrade)
+        {
+            c4.SetActive(false);
+            ear.transform.localPosition = Vector3.zero;
+            esold = true;
+            ebutton.interactable = false;
+            GoldCounter.instance.DecreaseGold(int.Parse(ear.text));
+            ear.text = "Sold";
+            upgrade= false;
+        }
+        else
+        {
+            displayPopUp = true;
+        }
     }
 
     public void DungButton()
     {
-        c5.SetActive(false);
-        dung.text = "Sold";
-        dung.transform.localPosition = Vector3.zero;
-        dsold = true;
-        dbutton.interactable = false;
+        CheckGold(int.Parse(dung.text));
+        if (upgrade)
+        {
+            c5.SetActive(false);
+            dung.transform.localPosition = Vector3.zero;
+            dsold = true;
+            dbutton.interactable = false;
+            GoldCounter.instance.DecreaseGold(int.Parse(dung.text));
+            dung.text = "Sold";
+            upgrade = false;
+        }
+        else
+        {
+            displayPopUp = true;
+        }
+    }
+
+    private void CheckGold( int value)
+    {
+        if (GoldCounter.instance.currentCount >= value) 
+        {
+            upgrade = true;
+        }
+    }
+
+    private void ResetPopUp()
+    {
+        backgroundColor.a = 1;
+        textColor.a = 1;
+    }
+
+    private IEnumerator PopUp()
+    {
+        yield return new WaitForSeconds(0.1f);
+        backgroundColor.a = backgroundColor.a - 0.5f * Time.deltaTime;
+        textColor.a = textColor.a - 0.5f * Time.deltaTime;
+    }
+
+    private void BrokePopUp()
+    {
+        popUp.SetActive(true);
+        if (!resetPop)
+        {
+            ResetPopUp();
+        }
+        resetPop = true;
+        StartCoroutine(PopUp());
+        if (backgroundColor.a <= 0 && textColor.a <= 0)
+        {
+            resetPop = false;
+            displayPopUp = false;
+            popUp.SetActive(false);
+        }
     }
 }
